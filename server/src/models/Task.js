45c50b3063e87e16b6dcbcd4393739b5636285b0
David Kostuchenko <davidkost1999@gmail.com>
2022-02-12 16:26:09 +0200

    [Task model]

---
+++ server/src/models/Task.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const TaskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", TaskSchema);
+
+export default Task;
