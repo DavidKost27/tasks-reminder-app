565e77a32c9c06228c2703b83d5d98676c0d6b86
David Kostuchenko <davidkost1999@gmail.com>
2022-02-12 11:39:51 +0200

    [Established a connection with the mongodb cluster]

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,9 @@ if ((process.env.NODE_ENV = "development")) {
   require("dotenv").config({ path: path.join(__dirname, "../.env") });
 }
 
-// Variables
+// Requirements
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Initialize express app
 const app = express();
@@ -13,12 +14,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Variables
+const PORT = process.env.PORT || 3001;
+const uri = process.env.MONGODB_URI;
+
+// Connect mongodb
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => console.log(`connected to db...`))
+  .catch((err) => console.log(err));
+//
+
 // Router index
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
 
 //Express listener
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
